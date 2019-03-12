f759a9e9119c38871298f4b9c90a525ff9ff2b72
André Araujo <aaraujo1@my.wctc.edu>
2019-03-12 10:39:06 -0500

    Started Lab: Personality Quiz

---
+++ PersonalityQuiz/PersonalityQuiz/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PersonalityQuiz
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
